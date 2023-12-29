b241ce3886f9c24d76554e34072a66a382e557c7
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2023-12-30 00:05:38 +0100

    more conversion tests

--- conversion_test.go
+++ conversion_test.go
@@ -24,12 +24,17 @@ func TestIdempotent(t *testing.T) {
 	t.Parallel()
 	v4 := mustAddr("0.0.0.0")
 	if back(peek(v4)) != v4 {
-		t.Fatalf("back(peek(ip)) isn't idempotent")
+		t.Fatalf("back(peek(ip)) isn't idempotent, expect: %v, got: %v", v4, back(peek(v4)))
 	}
 
 	v6 := mustAddr("::")
 	if back(peek(v6)) != v6 {
-		t.Fatalf("back(peek(ip)) isn't idempotent")
+		t.Fatalf("back(peek(ip)) isn't idempotent, expect: %v, got: %v", v6, back(peek(v6)))
+	}
+
+	v4mappedv6 := mustAddr("::ffff:127.0.0.1")
+	if back(peek(v4mappedv6)) != v4mappedv6 {
+		t.Fatalf("back(peek(ip)) isn't idempotent, expect: %v, got: %v", v4mappedv6, back(peek(v4mappedv6)))
 	}
 }
 
@@ -47,6 +52,8 @@ func TestModify(t *testing.T) {
 		t.Fatalf("peek -> sub one -> back not as expected")
 	}
 
+	// --
+
 	p6 := peek(mustAddr("::"))
 	p6.ip.lo++ // add one
 
@@ -56,6 +63,20 @@ func TestModify(t *testing.T) {
 
 	p6.ip.lo-- // sub one
 	if back(p6) != mustAddr("::") {
+		t.Fatalf("peek -> sub one -> back not as expected")
+	}
+
+	// --
+
+	v4mappedv6 := peek(mustAddr("::ffff:127.0.0.0"))
+	v4mappedv6.ip.lo-- // sub one
+
+	if back(v4mappedv6) != mustAddr("::ffff:126.255.255.255") {
+		t.Fatalf("peek -> add one -> back not as expected")
+	}
+
+	v4mappedv6.ip.lo++ // add one
+	if back(v4mappedv6) != mustAddr("::ffff:127.0.0.0") {
 		t.Fatalf("peek -> sub one -> back not as expected")
 	}
 }

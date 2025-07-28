521397cfff5a40019a65604f1f37e044da5b9f91
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-07-28 17:56:03 +0200

    make it faster for IPv4, add more benchmarks

--- conversion.go
+++ conversion.go
@@ -11,24 +11,29 @@ type addr struct {
 	is4 bool
 }
 
-func peek(a netip.Addr) addr {
-	var b addr
-	b.is4 = a.Is4()
+func peek(a netip.Addr) (b addr) {
+	ip := a.AsSlice()
 
-	a16 := a.As16()
-	b.ip.hi = binary.BigEndian.Uint64(a16[:8])
-	b.ip.lo = binary.BigEndian.Uint64(a16[8:])
+	if len(ip) == 4 {
+		b.is4 = true
+		b.ip.lo = uint64(binary.BigEndian.Uint32(ip))
+		return b
+	}
+
+	b.ip.hi = binary.BigEndian.Uint64(ip[:8])
+	b.ip.lo = binary.BigEndian.Uint64(ip[8:])
+
 	return b
 }
 
 func back(a addr) netip.Addr {
 	var a16 [16]byte
-	binary.BigEndian.PutUint64(a16[:8], a.ip.hi)
 	binary.BigEndian.PutUint64(a16[8:], a.ip.lo)
 
 	if a.is4 {
 		return netip.AddrFrom4([4]byte(a16[12:]))
 	}
 
+	binary.BigEndian.PutUint64(a16[:8], a.ip.hi)
 	return netip.AddrFrom16(a16)
 }

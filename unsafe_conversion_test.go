11e418027ce8ddd442af90baa2b4b471cbe2860f
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2025-07-29 23:02:05 +0200

    just naming and comments

--- unsafe_conversion_test.go
+++ unsafe_conversion_test.go
@@ -22,60 +22,60 @@ var (
 func TestIdempotent(t *testing.T) {
 	t.Parallel()
 	v4 := mustAddr("0.0.0.0")
-	if back(peek(v4)) != v4 {
-		t.Fatalf("back(peek(ip)) isn't idempotent, expect: %v, got: %v", v4, back(peek(v4)))
+	if wrap(unwrap(v4)) != v4 {
+		t.Fatalf("wrap(unwrap(ip)) isn't idempotent, expect: %v, got: %v", v4, wrap(unwrap(v4)))
 	}
 
 	v6 := mustAddr("::")
-	if back(peek(v6)) != v6 {
-		t.Fatalf("back(peek(ip)) isn't idempotent, expect: %v, got: %v", v6, back(peek(v6)))
+	if wrap(unwrap(v6)) != v6 {
+		t.Fatalf("wrap(unwrap(ip)) isn't idempotent, expect: %v, got: %v", v6, wrap(unwrap(v6)))
 	}
 
 	v4mappedv6 := mustAddr("::ffff:127.0.0.1")
-	if back(peek(v4mappedv6)) != v4mappedv6 {
-		t.Fatalf("back(peek(ip)) isn't idempotent, expect: %v, got: %v", v4mappedv6, back(peek(v4mappedv6)))
+	if wrap(unwrap(v4mappedv6)) != v4mappedv6 {
+		t.Fatalf("wrap(unwrap(ip)) isn't idempotent, expect: %v, got: %v", v4mappedv6, wrap(unwrap(v4mappedv6)))
 	}
 }
 
 func TestModify(t *testing.T) {
 	t.Parallel()
-	p4 := peek(mustAddr("0.0.0.0"))
+	p4 := unwrap(mustAddr("0.0.0.0"))
 	p4.ip.lo++ // add one
 
-	if back(p4) != mustAddr("0.0.0.1") {
-		t.Fatalf("peek -> add one -> back not as expected")
+	if wrap(p4) != mustAddr("0.0.0.1") {
+		t.Fatalf("unwrap -> add one -> wrap not as expected")
 	}
 
 	p4.ip.lo-- // sub one
-	if back(p4) != mustAddr("0.0.0.0") {
-		t.Fatalf("peek -> sub one -> back not as expected")
+	if wrap(p4) != mustAddr("0.0.0.0") {
+		t.Fatalf("unwrap -> sub one -> wrap not as expected")
 	}
 
 	// --
 
-	p6 := peek(mustAddr("::"))
+	p6 := unwrap(mustAddr("::"))
 	p6.ip.lo++ // add one
 
-	if back(p6) != mustAddr("::1") {
-		t.Fatalf("peek -> add one -> back not as expected")
+	if wrap(p6) != mustAddr("::1") {
+		t.Fatalf("unwrap -> add one -> wrap not as expected")
 	}
 
 	p6.ip.lo-- // sub one
-	if back(p6) != mustAddr("::") {
-		t.Fatalf("peek -> sub one -> back not as expected")
+	if wrap(p6) != mustAddr("::") {
+		t.Fatalf("unwrap -> sub one -> wrap not as expected")
 	}
 
 	// --
 
-	v4mappedv6 := peek(mustAddr("::ffff:127.0.0.0"))
+	v4mappedv6 := unwrap(mustAddr("::ffff:127.0.0.0"))
 	v4mappedv6.ip.lo-- // sub one
 
-	if back(v4mappedv6) != mustAddr("::ffff:126.255.255.255") {
-		t.Fatalf("peek -> add one -> back not as expected")
+	if wrap(v4mappedv6) != mustAddr("::ffff:126.255.255.255") {
+		t.Fatalf("unwrap -> add one -> wrap not as expected")
 	}
 
 	v4mappedv6.ip.lo++ // add one
-	if back(v4mappedv6) != mustAddr("::ffff:127.0.0.0") {
-		t.Fatalf("peek -> sub one -> back not as expected")
+	if wrap(v4mappedv6) != mustAddr("::ffff:127.0.0.0") {
+		t.Fatalf("unwrap -> sub one -> wrap not as expected")
 	}
 }
